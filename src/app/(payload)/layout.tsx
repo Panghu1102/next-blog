28c32aa8d85265f4200bb34ee8d120c8963e8341
Panghu1102 <Ph101102@163.com>
2026-09-13 11:10:03 +0800

    fix: add Payload server function to admin layout

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -1,11 +1,33 @@
-import config from "@payload-config";
-import { RootLayout } from "@payloadcms/next/layouts";
-import "@payloadcms/next/css";
-import "./custom.scss";
-import { importMap } from "./admin/importMap";
+import type { ServerFunctionClient } from 'payload'
 
-export default function PayloadLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return RootLayout({ children, config, importMap });
+import '@payloadcms/next/css'
+
+import config from '@payload-config'
+import { handleServerFunctions, RootLayout } from '@payloadcms/next/layouts'
+import React from 'react'
+
+import { importMap } from './admin/importMap'
+
+import './custom.scss'
+
+type Args = {
+  children: React.ReactNode
 }
+
+const serverFunction: ServerFunctionClient = async function (args) {
+  'use server'
+
+  return handleServerFunctions({
+    ...args,
+    config,
+    importMap,
+  })
+}
+
+const Layout = ({ children }: Args) => (
+  <RootLayout config={config} importMap={importMap} serverFunction={serverFunction}>
+    {children}
+  </RootLayout>
+)
+
+export default Layout
