2f3a238d21f08e50eab024f3690610231cf46537
Panghu1102 <Ph101102@163.com>
2026-09-13 12:00:31 +0800

    fix: let OpenNext shim handle @next/env namespace import

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ import { withPayload } from "@payloadcms/next/withPayload";
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["@payloadcms/db-d1-sqlite", "jose", "@next/env"],
+  serverExternalPackages: ["@payloadcms/db-d1-sqlite", "jose"],
 };
 
 export default withNextIntl(withPayload(nextConfig));
